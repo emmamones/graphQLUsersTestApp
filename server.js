9e8f9aa118cdb90678a3b9a6d6e5547ed6fd5eeb
Emmanuel-laptop-work <punky_casinotomo@hotmail.com>
2019-08-14 09:55:08 +0200

    cleaning changes from laptop

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/graphql',expressGraphQL({
 //its a development tool and its only intended for development enviroment
 
 app.get('/hi',(req,res)=>{
-  return  console.log('hellomotherFucker');
+  return  console.log('hello');
     
 })
 app.listen(_port,()=>{
